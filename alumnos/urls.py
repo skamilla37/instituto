157b34277f497b1d7029a15b5dedc223362e3cc4
skamilla37 <ce.escamilla@duocuc.cl>
2024-06-22 12:28:12 -0400

    primer commit

--- alumnos/urls.py
+++ alumnos/urls.py
@@ -1,6 +1,14 @@
 from django.urls import path
-from .views import index
+from .views import index, crud, alumnosAdd, alumnos_del, alumnos_findEdit, alumnosUpdate
 
 urlpatterns = [
-    path('', index, name='index')
+    path('', index, name='index'),
+    path('crud', crud, name='crud'),
+    path('alumnosAdd', alumnosAdd, name='alumnosAdd'),
+    path('alumnos_del/<str:pk>', alumnos_del, name='alumnos_del'),
+    path('alumnos_findEdit/<str:pk>', alumnos_findEdit, name='alumnos_findEdit'),
+    path('alumnosUpdate', alumnosUpdate, name='alumnosUpdate'),
+
+
 ]
+
--- alumnos/views.py
+++ alumnos/views.py
@@ -20,3 +20,115 @@ def index(request):
     context={"hijo":hijo, "nombre":"Claudia Andrea", "comidas":lista, "alumnos":alumnos }
 
     return render(request, 'alumnos/index.html', context)
+def crud(request):
+    alumnos = Alumno.objects.all()
+    context = {'alumnos': alumnos}
+    return render(request,'alumnos/alumnos_list.html', context)
+
+def alumnosAdd(request):
+    if request.method != 'POST':
+
+        generos = Genero.objects.all()
+        context = {'generos': generos}
+        return render(request, 'alumnos/alumnos_add.html', context)
+    elif request.method == 'POST':
+
+        rut=request.POST['rut']
+        nombre=request.POST['nombre']
+        aPaterno=request.POST['paterno']
+        aMaterno=request.POST['materno']
+        fechaNac=request.POST['fechaNac']
+        genero=request.POST['genero']
+        telefono=request.POST['telefono']
+        email=request.POST['email']
+        direccion=request.POST['direccion']
+        activo="1"
+
+        objGenero = Genero.objects.get(id_genero = genero)
+        obj=Alumno.objects.create(  rut=rut, 
+                                    nombre=nombre, 
+                                    apellido_paterno=aPaterno, 
+                                    apellido_materno=aMaterno, 
+                                    fecha_nacimiento=fechaNac, 
+                                    id_genero=objGenero, 
+                                    telefono=telefono, 
+                                    email=email, 
+                                    direccion=direccion, 
+                                    activo=1)
+        obj.save()
+        context = {'mensaje': 'Ok, datos guardados...'}
+        return render(request, 'alumnos/alumnos_add.html', context)
+
+def alumnos_del(request, pk):
+    context = {}
+    try:
+        alumno = Alumno.objects.get(rut=pk)
+
+        alumno.delete()
+        mensaje = 'Ok, registro eliminado...'
+        alumnos = Alumno.objects.all()
+        context = {'alumnos': alumnos, 'mensaje': mensaje}
+        return render(request, 'alumnos/alumnos_list.html', context)
+    except:
+        mensaje='Error, rut no existe...'
+        alumnos = Alumno.objects.all()
+        context = {'alumnos': alumnos, 'mensaje': mensaje}
+        return render(request, 'alumnos/alumnos_list.html', context)
+    
+def alumnos_findEdit(request, pk):
+
+    if pk != "":
+        alumno = Alumno.objects.get(rut=pk)
+        generos = Genero.objects.all()
+
+        print(type(alumno.id_genero.genero))
+
+        context = {'alumno': alumno, 'generos': generos}
+        if alumno:
+            return render(request, 'alumnos/alumnos_edit.html', context)
+        else:
+            context={'mensaje':'Error, rut no existe...'}
+        return render(request, 'alumnos/alumnos_list.html', context)
+    
+def alumnosUpdate(request):
+
+    if request.method == "POST":
+        
+        rut=request.POST['rut']
+        nombre=request.POST['nombre']
+        aPaterno=request.POST['paterno']
+        aMaterno=request.POST['materno']
+        fechaNac=request.POST['fechaNac']
+        genero=request.POST['genero']
+        telefono=request.POST['telefono']
+        email=request.POST['email']
+        direccion=request.POST['direccion']
+        activo="1"
+
+        objGenero = Genero.objects.get(id_genero = genero)
+
+        alumno =Alumno()
+        alumno.rut=rut
+        alumno.nombre=nombre
+        alumno.apellido_paterno=aPaterno
+        alumno.apellido_materno=aMaterno
+        alumno.fecha_nacimiento=fechaNac
+        alumno.id_genero=objGenero
+        alumno.telefono=telefono
+        alumno.email=email
+        alumno.direccion=direccion
+        alumno.activo=1
+        alumno.save()
+        
+        generos=Genero.objects.all()
+        context = {'generos': generos, 'mensaje': 'Ok, datos actualizados...', 'alumno': alumno}
+        return render(request, 'alumnos/alumnos_edit.html', context)
+    else:
+        alumnos = Alumno.objects.all()
+        context = {'alumnos': alumnos}
+        return render(request, 'alumnos/alumnos_list.html', context)  
+
+        
+
+
+        
